2bdcbcecdb87d8a2c5f4da8a5e868a9dd291c13f
neuervinch <npandian515@gmail.com>
2025-02-14 09:24:15 +0530

    parakurom

---
+++ bk/Models/UserModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    rollNo : {
+        type: Number,
+        required: [true,"RollNo is required"],
+        unique : true,
+        trim : true,
+    },
+
+    password: {
+        type : String,
+        required : [true,"Password is required"],
+        trim : true,
+        select : false,
+    },
+
+     email : {
+        type : String,
+        required : [true,"Email is required"],
+        unique : true,
+        trim : true,
+    },
+    
+    roles : {
+        type : String,
+        enum :['student','staff' ,'hod'],
+        default: 'student',
+    },
+
+    department : {
+        type : String
+    },
+
+    class : {
+        type : String
+    },
+    timetable : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'TimetableModel'
+        }
+    ] 
+    
+
+
+
+
+},{
+    timestamps : true,
+})
+
+module.exports = mongoose.model('UserModel',userSchema);
